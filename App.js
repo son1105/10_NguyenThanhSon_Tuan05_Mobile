8a133f46d29571d591f0a99823fb4a0c10496577
Sơn Nguyễn <20106421.son@student.iuh.edu.vn>
2023-10-13 22:31:10 +0700

    finish week05

--- App.js
+++ App.js
@@ -1,146 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { Entypo, MaterialIcons, FontAwesome } from '@expo/vector-icons';
-import pickColor from './screens/pickcolor'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Pickcolor from './screens/Pickcolor'
+import Home from './screens/HomeScreen'
 
+const Stack = createNativeStackNavigator();
 export default function App() {
     return (
-        pickColor()
-        // <View style={styles.container}>
-        //     <View style={styles.viewImg}>
-        //         <Image style={styles.image} source={require('./assets/blue.png')} />
-        //     </View>
-        //     <View style={styles.viewInfo}>
-        //         <Text style={styles.textTitle}>Điện Thoại Vsmart Joy 3 - Hàng chính hãng</Text>
-        //         <View style={styles.viewRating}>
-        //             <View style={styles.viewStar}>
-        //             <Entypo name="star" size={24} color="yellow" />
-        //             <Entypo name="star" size={24} color="yellow" />
-        //             <Entypo name="star" size={24} color="yellow" />
-        //             <Entypo name="star" size={24} color="yellow" />
-        //             <Entypo name="star" size={24} color="yellow" />
-        //             </View>
-        //             <Text style={styles.textTitle}>(Xem 828 đánh giá)</Text>
-        //         </View>
-        //         <View style={styles.viewPrice}>
-        //             <Text style={styles.textPriceSale}>1.790.000 đ</Text>
-        //             <Text style={styles.textPrice}>1.790.000 đ</Text>
-        //         </View>
-        //         <View style={styles.viewRefund}>
-        //         <Text style={styles.textRefund}>Ở ĐÂU RẺ HƠN HOÀN TIỀN</Text>
-        //         <FontAwesome name="question-circle-o" size={24} color="black" />
-        //         </View>
-        //         <View style={styles.viewPickColor}>
-        //             <Text style={styles.textPickColor}>4 MÀU-CHỌN MÀU</Text>
-        //             <MaterialIcons style={styles.iconPickColor} name="navigate-next" size={24} color="black" />
-        //         </View>
-        //     </View>
-        //     <View style={styles.viewButton}>
-        //         <TouchableOpacity style={styles.button}>
-        //             <Text style={styles.textButton}>CHỌN MUA</Text>
-        //         </TouchableOpacity>
-        //     </View>
-
-        // </View>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Pick Color" component={Pickcolor} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    viewImg: {
-        flex: 3,
-        width: '90%',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    image: {
-        width: '75%',
-        height: '100%'
-    },
-    viewInfo: {
-        flex: 2,
-        width: '90%',
-    },
-    textTitle: {
-        marginVertical: 10,
-        fontWeight: 400,
-        fontSize: '15px',
-        fontWeight: 400,
-    },
-    viewRating: {
-        width: '80%',
-        flexDirection: 'row',
-        marginBottom: 10,
-        alignItems: 'center',
-        justifyContent: 'space-between',
-    },
-    viewStar:{
-        flexDirection: 'row'
-    },
-    viewPrice: {
-        width: '70%',
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-    textPriceSale:{
-        fontWeight: 700,
-        fontSize: 18
-    },
-    textPrice:{
-        fontWeight: 700,
-        fontSize: 15,
-        textDecorationLine: 'line-through'
-    },
-    viewRefund:{
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    textRefund:{
-        marginVertical: 20,
-        marginRight: 10,
-        fontWeight: 700,
-        fontSize: 12,
-        color: '#FA0000'
-    },
-    viewPickColor:{
-        height: '20%',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        borderWidth: 1,
-        borderRadius: 10
-    },
-    textPickColor:{
-        width: '90%',
-        paddingLeft: '10%',
-        textAlign: 'center',
-        fontWeight: 400,
-        fontSize: 15
-    },
-    viewButton: {
-        flex: 1,
-        width: '90%',
-        justifyContent: 'flex-end',
-    },
-    button: {
-        width: '100%',
-        height: '50%',
-        marginBottom: '10px',
-        backgroundColor: '#EE0A0A',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: '10px',
-        borderWidth: 1
-    },
-    textButton: {
-        fontFamily: 'Roboto' ,
-        color: '#F9F2F2',
-        fontSize: 20,
-        fontWeight: 700
-    }
-});
--- screens/pickcolor.js
+++ screens/pickcolor.js
@@ -1,22 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
+import {useState} from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import { Entypo, MaterialIcons, FontAwesome } from '@expo/vector-icons';
 
-export default function App() {
+export default function App({route, navigation  }) {
+    var checkColor = route.params
+    var [color, setColor] = useState(checkColor?
+        (checkColor == 'black.PNG'?'đen':
+        checkColor == 'blue.PNG'?'xanh':
+        checkColor == 'red.PNG'?'đỏ':'bạc')
+        :'xanh'); //['black.PNG','blue.PNG','red.PNG','silver.PNG'
+    var [image, setImage] = useState(route.params);
     return (
         <View style={styles.container}>
             <View style={styles.viewInfo}>
-                <Image style={styles.img} source={require('../assets/blue.png')}/>
+                <Image style={styles.img} source={require(`../assets/${image}`)}/>
                 <View style={styles.viewInfoText}>
-                    <Text>Điện Thoại Vsmart Joy 3
+                    <Text style={styles.textInfo}>Điện Thoại Vsmart Joy 3
                         {'\n'}Hàng chính hãng</Text>
-                    <Text>Màu đỏ</Text>
-                    <Text>Cung cấp bởi Tiki Tradding</Text>
-                    <Text>1.790.000 đ</Text>
+                    <Text style={styles.textInfo}>Màu: <Text style={[styles.textInfo, {fontWeight:700}]}>{color}</Text></Text>
+                    <Text style={styles.textInfo}>Cung cấp bởi <Text style={[styles.textInfo, {fontWeight:700}]}>Tiki Tradding</Text></Text>
+                    <Text style={styles.textPrice}>1.790.000 đ</Text>
                 </View>
             </View>
             <View style={styles.viewColors}>
-
+                <Text style={styles.textColor}>Chọn một màu bên dưới:</Text>
+                <View style={styles.viewRect}>
+                <TouchableOpacity style={[styles.rectangle,{backgroundColor:'#C5F1FB'}]}
+                    onPress={()=>{
+                        setImage('silver.PNG');
+                        setColor('bạc');
+                    }}
+                />
+                <TouchableOpacity style={[styles.rectangle,{backgroundColor:'#F30D0D'}]}
+                    onPress={()=>{
+                        setImage('red.PNG');
+                        setColor('đỏ');
+                    }}
+                />
+                <TouchableOpacity style={[styles.rectangle,{backgroundColor:'#000000'}]}
+                    onPress={()=>{
+                        setImage('black.PNG');
+                        setColor('đen');
+                    }}
+                />
+                <TouchableOpacity style={[styles.rectangle,{backgroundColor:'#234896'}]}
+                    onPress={()=>{
+                        setImage('blue.PNG');
+                        setColor('xanh');
+                    }}
+                />
+                </View>
+                <View style={styles.viewButton}>
+                <TouchableOpacity style={styles.button}
+                    onPress={()=>{navigation.navigate('Home',image)}}
+                >
+                    <Text style={styles.textButton}>Xong</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     );
@@ -26,19 +65,77 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        // alignItems: 'center',
+        // justifyContent: 'center',
     },
     viewInfo:{
         flex: 1,
         flexDirection: 'row',
-        width: '100%'
+        width: '100%',
+        alignItems: 'center',
     },
     img:{
+        marginHorizontal: '2%',
         width:'30%',
         height: '90%'
     },
+    viewInfoText:{
+        height: '90%',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        marginLeft: '3%',
+    },
+    textInfo:{
+        // flex: 1,
+        fontWeight: 400,
+        fontSize: 15
+    },
+    textPrice:{
+        // flex: 1,
+        fontWeight: 700,
+        fontSize: 18
+    },
     viewColors:{
-        flex: 3
-    }
+        flex: 3,
+        backgroundColor: '#C4C4C4'
+    },
+    textColor:{
+        flex: 1,
+        marginHorizontal: '5%',
+        marginTop: '2%',
+        fontWeight: 400,
+        fontSize: 18
+    },
+    viewRect:{
+        flex: 10,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+    rectangle:{
+        width: '20%',
+        height: '20%',
+        marginVertical: '2%',
+        shadowColor: '#000000',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.1,
+    },
+    viewButton:{
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    button:{
+        width: '90%',
+        height: '60%',
+        backgroundColor: '#1952E2',
+        borderWidth: 1,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    textButton:{
+        color: '#fff',
+        fontWeight: 700,
+        fontSize: 20
+    },
 });
